67adc6140bf83372d80336dfea38cc273c7a567e
rshellnet <rouslan@rshell.net>
2025-08-27 16:30:30 +0300

    Update custom.sh

--- resources/custom.sh
+++ resources/custom.sh
@@ -5,7 +5,7 @@ set -exu
 # Add your additional provisioning here for custom VM images.
 
 install_build_packages() {
-  ASSUME_ALWAYS_YES=yes pkg install git go121
+  ASSUME_ALWAYS_YES=yes pkg install git go121 bash
 }
 
 install_build_packages
